d8dce137976173372ef1d1461652496f2f6cb7a6
dsmithpadilla <88508950+dsmithpadilla@users.noreply.github.com>
2022-11-17 18:15:48 -0500

    Update GeckoView (Beta) to 108.0.20221117185908.

--- android-components/buildSrc/src/main/java/Gecko.kt
+++ android-components/buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "108.0.20221115200658"
+    const val version = "108.0.20221117185908"
 
     /**
      * GeckoView channel
