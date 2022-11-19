4fe1b24d54b8625d7a616fee71d7b4a010b75741
Gabriel Luong <gabriel.luong@gmail.com>
2022-11-18 20:49:06 -0500

    test

--- android-components/plugins/dependencies/src/main/java/Gecko.kt
+++ android-components/plugins/dependencies/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "109.0.20221118154632"
+    const val version = "109.0.20221118094451"
 
     /**
      * GeckoView channel
