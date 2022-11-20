fa98d33474d7264cc16120d53df74ce2080d85ed
MickeyMoz <sebastian@mozilla.com>
2022-11-20 22:05:28 +0000

    Update GeckoView (Beta) to 108.0.20221120185746.

--- android-components/buildSrc/src/main/java/Gecko.kt
+++ android-components/buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "108.0.20221118185748"
+    const val version = "108.0.20221120185746"
 
     /**
      * GeckoView channel
