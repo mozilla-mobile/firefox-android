bb255703e89a96d45b4e0a85861f49394b03493b
Bruno Rosa <rosahbruno@gmail.com>
2023-02-23 09:24:42 -0500

    Bug 1817029 - add the GleanTestRule for tests

--- fenix/app/src/test/java/org/mozilla/fenix/components/metrics/clientdeduplication/ClientDeduplicationPingTest.kt
+++ fenix/app/src/test/java/org/mozilla/fenix/components/metrics/clientdeduplication/ClientDeduplicationPingTest.kt
@@ -4,13 +4,23 @@
 
 package org.mozilla.fenix.components.metrics.clientdeduplication
 
+import mozilla.components.service.glean.testing.GleanTestRule
+import mozilla.components.support.test.robolectric.testContext
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mozilla.fenix.GleanMetrics.ClientDeduplication
 import org.mozilla.fenix.GleanMetrics.Pings
+import org.mozilla.fenix.helpers.FenixRobolectricTestRunner
 
+// For gleanTestRule
+@RunWith(FenixRobolectricTestRunner::class)
 internal class ClientDeduplicationPingTest {
+    @get:Rule
+    val gleanTestRule = GleanTestRule(testContext)
+
     @Test
     fun `The clientDeduplication ping is sent`() {
         // Record test data.
