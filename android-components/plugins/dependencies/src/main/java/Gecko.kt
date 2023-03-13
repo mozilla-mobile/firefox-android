7b1dcf0ef4f3ea6ad730c29b8fe25804437ccfcd
dsmithpadilla <88508950+dsmithpadilla@users.noreply.github.com>
2023-03-13 18:31:26 -0400

    Update to GeckoView Beta 112.0.20230313153916 on releases_v112
    This patch manually updates GV Beta to 112.0.20230313153916.

--- android-components/plugins/dependencies/src/main/java/Gecko.kt
+++ android-components/plugins/dependencies/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "112.0.20230312211644"
+    const val version = "112.0.20230313153916"
 
     /**
      * GeckoView channel
