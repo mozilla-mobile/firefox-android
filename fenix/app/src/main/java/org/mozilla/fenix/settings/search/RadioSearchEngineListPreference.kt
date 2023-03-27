3c8afe6d718d38568547357ec828366b8c0090a0
alexandra.virvara <avirvara@mozilla.com>
2023-03-27 09:35:07 +0300

    Bug 1823085 - NoSuchElementException fix - the search engines list does not contain any element matching the conditions, so calling first on that list will throw an exception

--- fenix/app/src/main/java/org/mozilla/fenix/settings/search/RadioSearchEngineListPreference.kt
+++ fenix/app/src/main/java/org/mozilla/fenix/settings/search/RadioSearchEngineListPreference.kt
@@ -171,16 +171,30 @@ class RadioSearchEngineListPreference @JvmOverloads constructor(
     ) {
         val selectedOrDefaultSearchEngine = context.components.core.store.state.search.selectedOrDefaultSearchEngine
         if (selectedOrDefaultSearchEngine == engine) {
-            val nextSearchEngine = if (context.settings().showUnifiedSearchFeature) {
-                context.components.core.store.state.search.searchEngines.first {
-                    it.id != engine.id && (it.isGeneral || it.type == SearchEngine.Type.CUSTOM)
+            val nextSearchEngine = try {
+                if (context.settings().showUnifiedSearchFeature) {
+                    context.components.core.store.state.search.searchEngines.first {
+                        it.id != engine.id && (it.isGeneral || it.type == SearchEngine.Type.CUSTOM)
+                    }
+                } else {
+                    context.components.core.store.state.search.searchEngines.first {
+                        it.id != engine.id
+                    }
                 }
-            } else {
-                context.components.core.store.state.search.searchEngines.first {
-                    it.id != engine.id
+            } catch (ex: NoSuchElementException) {
+                try {
+                    context.components.core.store.state.search.searchEngines.first {
+                        it.id != engine.id
+                    }
+                } catch (ex: NoSuchElementException) {
+                    null
                 }
             }
-            context.components.useCases.searchUseCases.selectSearchEngine(nextSearchEngine)
+            nextSearchEngine?.let {
+                context.components.useCases.searchUseCases.selectSearchEngine(
+                    nextSearchEngine,
+                )
+            }
         }
         context.components.useCases.searchUseCases.removeSearchEngine(engine)
 
