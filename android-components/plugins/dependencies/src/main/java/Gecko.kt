ba51cc4285fdb42954939705fecda7908c7ae0a8
MickeyMoz <sebastian@mozilla.com>
2023-08-15 21:19:14 +0000

    Update GeckoView (Release) to 116.0.20230815173142.

--- android-components/plugins/dependencies/src/main/java/Gecko.kt
+++ android-components/plugins/dependencies/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "116.0.20230811183303"
+    const val version = "116.0.20230815173142"
 
     /**
      * GeckoView channel
