9651b64ffe1559623a8223c1211a78536d365d77
oana.horvath <ohorvath@mozilla.com>
2023-08-21 17:01:27 +0300

    Bug 1849278 - Disable cookies reduction banner in SearchTest

--- fenix/app/src/androidTest/java/org/mozilla/fenix/ui/ComposeSearchTest.kt
+++ fenix/app/src/androidTest/java/org/mozilla/fenix/ui/ComposeSearchTest.kt
@@ -58,6 +58,7 @@ class ComposeSearchTest {
             isRecentTabsFeatureEnabled = false,
             isTCPCFREnabled = false,
             isWallpaperOnboardingEnabled = false,
+            isCookieBannerReductionDialogEnabled = false,
             tabsTrayRewriteEnabled = true,
         ),
     ) { it.activity }
