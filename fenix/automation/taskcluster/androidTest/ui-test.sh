403b042767d75c23c729eacbc078f34de1b357e2
Jackie Johnson <107960801+jjSDET@users.noreply.github.com>
2023-09-07 09:47:48 -0500

    print env vars

--- fenix/automation/taskcluster/androidTest/ui-test.sh
+++ fenix/automation/taskcluster/androidTest/ui-test.sh
@@ -94,6 +94,7 @@ APK_TEST="$(get_abs_filename $APK_TEST)"
 echo "device_type: ${device_type}"
 echo "APK_APP: ${APK_APP}"
 echo "APK_TEST: ${APK_TEST}"
+echo "ENV_VARS: "; env
 
 # function to exit script with exit code from test run.
 # (Only 0 if all test executions passed)
