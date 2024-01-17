mozilla-mobile/firefox-android

commit daf1e8757f21e1e5bbec4a665e6f610eec76c15a

Remove references to service-telemetry from taskcluster

taskcluster/android_taskgraph/transforms/external_gradle_dependencies.py
@@ -55,8 +55,6 @@ def _get_build_gradle_paths(gradle_project):
 
     if gradle_project in ("focus", "fenix"):
         project_subdir = "app"
-    elif gradle_project == "service-telemetry":
-        project_subdir = "service-telemetry"
     else:
         project_subdir = get_path(gradle_project)
 
@@ -78,7 +76,7 @@ def _get_build_gradle_paths(gradle_project):
 
 
 def _get_gradle_project_dir(gradle_project):
-    if gradle_project in ("focus", "service-telemetry"):
+    if gradle_project in ("focus"):
         project_dir = "focus-android"
     elif gradle_project == "fenix":
         project_dir = "fenix"

taskcluster/android_taskgraph/transforms/gradle_optimization.py
@@ -81,8 +81,6 @@ def extend_optimization_if_one_already_exists(config, tasks):
 def _get_path(gradle_project):
     if gradle_project == "focus":
         return "focus-android/**"
-    elif gradle_project == "service-telemetry":
-        return "focus-android/service-telemetry/**"
     elif gradle_project == "fenix":
         return "fenix/**"
     else:

commit 42b68eeb9497691858b01cf2c6fe3f027bee10c6

Bug 1873858 - fix external-gradle-dependencies gradle project dir for focus
Fixes: daf1e8757 "Remove references to service-telemetry from taskcluster"

taskcluster/android_taskgraph/transforms/external_gradle_dependencies.py
@@ -76,7 +76,7 @@ def _get_build_gradle_paths(gradle_project):
 
 
 def _get_gradle_project_dir(gradle_project):
-    if gradle_project in ("focus"):
+    if gradle_project == "focus":
         project_dir = "focus-android"
     elif gradle_project == "fenix":
         project_dir = "fenix"