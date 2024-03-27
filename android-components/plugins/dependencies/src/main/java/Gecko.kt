b140c907ca25a8a91f1cde1307f99e0c7386d896
MickeyMoz <sebastian@mozilla.com>
2024-03-27 13:20:15 +0000

    Update GeckoView (Beta) to 125.0.20240327092250.

--- android-components/plugins/dependencies/src/main/java/Gecko.kt
+++ android-components/plugins/dependencies/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "125.0.20240325091408"
+    const val version = "125.0.20240327092250"
 
     /**
      * GeckoView channel
